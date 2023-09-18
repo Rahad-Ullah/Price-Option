69d41f7a69643772d754c91b12e097438d8e6b9e
Rahad-Ullah <Rahad-Ullah@users.noreply.github.com>
2023-09-18 17:26:11 +0600

    Website created successfully

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-4xl text-rose-500 font-bold text-center my-5'>Price Option</h1>
+      
+    </>
+  )
+}
+
+export default App
